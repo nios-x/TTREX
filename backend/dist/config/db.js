eb99d045432c2bb2d4a01ba3c0e9dd673acbafaf
nios-x <devnios7@gmail.com>
2025-09-02 12:24:38 +0530

    d

--- backend/dist/config/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../../src/generated/prisma");
-const client = new prisma_1.PrismaClient();
-exports.default = client;
--- backend/dist/routes/walletRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// routes/index.ts
-const express_1 = require("express");
-const walletController_1 = require("../controllers/walletController");
-const router = (0, express_1.Router)();
-router.post('/upsert', walletController_1.upsert);
-exports.default = router;
